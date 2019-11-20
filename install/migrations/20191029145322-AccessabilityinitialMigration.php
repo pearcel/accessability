ff4c646f7c68d65ff735064fbaa354e6a3116af5
Lawrie Pearce <lawrie.pearce@gmail.com>
2019-11-20 13:35:28 +1100

    Added tables to DB for customer input & store reply

--- install/migrations/20191029145322-AccessabilityinitialMigration.php
+++ install/migrations/20191029145322-AccessabilityinitialMigration.php
@@ -31,21 +31,47 @@ class AccessabilityinitialMigration extends CmfiveMigration {
 					->create();
 				}
 
+				if (!$this->hasTable("customer_input")) { //it can be helpful to check that the table name is not used
+					$this->table("customer_input", [ // table names should be appended with 'ModuleName_'
+						"id" => false,
+						"primary_key" => "id"
+					])->addColumn($column) // add the id column
+					->addStringColumn('name')
+					->addStringColumn('store_name')
+					
+					->addCmfiveParameters() // this function adds some standard columns used in cmfive. dt_created, dt_modified, creator_id, modifier_id, and is_deleted.
+					
+					->create();
+				}
 
+				if (!$this->hasTable("store_reply")) { //it can be helpful to check that the table name is not used
+					$this->table("store_reply", [ // table names should be appended with 'ModuleName_'
+						"id" => false,
+						"primary_key" => "id"
+					])->addColumn($column) // add the id column
+					->addStringColumn('name')
+					->addStringColumn('store_name')
+					->addStringColumn('comments')
 
+					->addCmfiveParameters() // this function adds some standard columns used in cmfive. dt_created, dt_modified, creator_id, modifier_id, and is_deleted.
+					
+					->create();
+				}
+			
 
 	}
 
 	public function down() {
 		// DOWN
 		$this->hasTable('accessability_issue') ? $this->dropTable('accessability_issue') : null;
+		$this->hasTable('customer_input') ? $this->dropTable('customer_input') : null;
+		$this->hasTable('store_reply') ? $this->dropTable('accessability_issue') : null;
 
-
-		
 	}
+	
+	
 
-	
-	
+
 
 	public function preText()
 	{
